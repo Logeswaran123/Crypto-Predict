7ccc864c2aad150e63b2710dcb734fd3fdb6d395
Loges <logiflash99037@gmail.com>
2023-01-30 23:17:23 +0530

    Add N-BEATS custom layer class

--- models.py
+++ models.py
@@ -9,6 +9,39 @@ from tensorflow.keras import layers
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
 
 
+# Create NBeatsBlock custom layer
+# Refer: https://arxiv.org/pdf/1905.10437.pdf (N-BEATS)
+class NBeatsBlock(tf.keras.layers.Layer):
+    def __init__(self,
+                input_size: int,
+                theta_size: int,
+                horizon: int,
+                num_hidden_units: int,
+                num_hidden_layers: int,
+                **kwargs):  # the **kwargs argument takes care of all of the arguments for the parent class (input_shape, trainable, name)
+        super().__init__(**kwargs)
+        self.input_size = input_size
+        self.theta_size = theta_size
+        self.horizon = horizon
+        self.num_hidden_units = num_hidden_units
+        self.num_hidden_layers = num_hidden_layers
+
+        self.hidden = [tf.keras.layers.Dense(self.num_hidden_units, activation="relu") for _ in range(self.num_hidden_layers)]
+
+        # Output of block is a theta layer with linear activation
+        self.theta_layer = tf.keras.layers.Dense(self.theta_size, activation="linear", name="theta")
+
+    def call(self, inputs):
+        x = inputs 
+        for layer in self.hidden:   # pass inputs through each hidden layer 
+            x = layer(x)
+        theta = self.theta_layer(x) 
+
+        # Output the backcast and forecast from theta
+        backcast, forecast = theta[:, :self.input_size], theta[:, -self.horizon:]
+        return backcast, forecast
+
+
 class Model():
     """
     Class with multiple model architectures
